c1fd85f6692ea80a16195985e71b0e855645e64e
B21020 <e1b21020@oit.ac.jp>
2024-06-17 10:42:57 +0900

    Main fix 1

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMONSTER GAME");
+    System.out.println("Show a new monster!");
+
+    Player player1 = new Player();
+    player1.drawMonsters();
+    player1.showDeck();
+
   }
 }
